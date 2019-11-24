54739962a578a7b5b2f31401c2f92fac04ebe322
Palinnilap <palinnilap@protonmail.com>
2019-11-24 10:37:24 -0500

    no grouping

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 class HappyGame(Widget):
+    HALF_GREY = [.5,.5,.5,1]
+    FULL_GREY = [1,1,1,1]
     button1 = ObjectProperty(None)
     button2 = ObjectProperty(None)
     button3 = ObjectProperty(None)
@@ -18,25 +20,21 @@ class HappyGame(Widget):
 
     def on_touch_down(self, touch):
         if self.button1.collide_point(*touch.pos):
-            self.button1.background_color = [.5,.5,.5,1]
-            print('button1 pushed')
+            self.button1.background_color = self.HALF_GREY
+            
         elif self.button2.collide_point(*touch.pos):
-            print('button2 pushed')
+            self.button2.background_color = self.HALF_GREY
         elif self.button3.collide_point(*touch.pos):
-            print('button3 pushed')
+            self.button3.background_color = self.HALF_GREY
         elif self.button4.collide_point(*touch.pos):
-            print('button4 pushed')
+            self.button4.background_color = self.HALF_GREY
     
     def on_touch_up(self, touch):
-        if self.button1.collide_point(*touch.pos):
-            self.button1.background_color = [1,1,1,1]
-            print('button1 pushed')
-        elif self.button2.collide_point(*touch.pos):
-            print('button2 pushed')
-        elif self.button3.collide_point(*touch.pos):
-            print('button3 pushed')
-        elif self.button4.collide_point(*touch.pos):
-            print('button4 pushed')
+        self.button1.background_color = self.FULL_GREY
+        self.button2.background_color = self.FULL_GREY
+        self.button3.background_color = self.FULL_GREY
+        self.button4.background_color = self.FULL_GREY
+            
 class HappyApp(App):
     def build(self):
         game = HappyGame()
